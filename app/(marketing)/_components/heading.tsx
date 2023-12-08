b931f71244a601a690c4a9fafcdc57b59a8eb19d
Hammad <hammadaslam382@gmail.com>
2023-12-08 21:52:17 +0500

    add marketing component part 2

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
-      <h1>
+      <h1 className="text-3xl sm:text5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plane. Unified. Welcome to{" "}
         <span className="underline">Jotion</span>
       </h1>
+      <h2 className="text-base sm:text-xl md:text-2xl font-medium">
+        Jotion is the connected workspace where <br />
+        better, faster work happens.
+      </h2>
+      <Button>
+        Enter Jotion
+        <ArrowRight />
+      </Button>
     </div>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
+import FooterPage from "./_components/footer";
 import Heading from "./_components/heading";
+import HerosPage from "./_components/heros";
 
 const MarketingPage = () => {
   return (
@@ -6,7 +8,9 @@ const MarketingPage = () => {
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         {" "}
         <Heading />
+        <HerosPage />
       </div>
+      <FooterPage />
     </div>
   );
 };

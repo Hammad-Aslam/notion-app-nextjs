31171005cc997c98cf53f9a78310dacbed855867
Hammad <hammadaslam382@gmail.com>
2023-12-09 19:26:23 +0500

    Done with database authentication

--- app/(marketing)/_components/NavBar.tsx
+++ app/(marketing)/_components/NavBar.tsx
@@ -5,6 +5,10 @@ import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const NavBar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -18,7 +22,27 @@ const NavBar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>{" "}
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,23 +1,35 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plane. Unified. Welcome to{" "}
         <span className="underline">Jotion</span>
       </h1>
-      <h2 className="text-base sm:text-xl md:text-2xl font-medium">
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where <br />
         better, faster work happens.
-      </h2>
-      <Button>
-        Enter Jotion
-        <ArrowRight />
-      </Button>
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button variant="ghost" size="sm">
+            Log in
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };

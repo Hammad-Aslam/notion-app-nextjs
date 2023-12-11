d2ffdcde1a6ecd416bee9f037f5987ee5325e39d
Hammad <hammadaslam382@gmail.com>
2023-12-11 19:36:03 +0500

    sidebar part 2

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,5 +1,18 @@
+"use client";
 const NavigationPage = () => {
-  return <>This is Navigation</>;
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+        <div>
+          <p>Action Items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover:opacity-100" />
+      </aside>
+    </>
+  );
 };
 
 export default NavigationPage;

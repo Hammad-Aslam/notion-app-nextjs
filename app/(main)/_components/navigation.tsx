5a84bb5a14537316d8a79921e19078b67154d425
Hammad <hammadaslam382@gmail.com>
2023-12-12 13:37:01 +0500

    Add userItem

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronLeft, Menu, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-items";
 const NavigationPage = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -103,7 +104,7 @@ const NavigationPage = () => {
         </div>
 
         <div>
-          <p>Action Items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
